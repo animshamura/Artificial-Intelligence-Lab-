80b8996c9215a7c5291d69146e0f61a6d4edb3b2
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2024-02-27 00:14:31 +0600

    Update Odd_Even_List.py

--- Lab 1/Odd_Even_List.py
+++ Lab 1/Odd_Even_List.py
@@ -1,6 +1,6 @@
 def Input():
      global container 
-     container = list(map(int,input("Enter tuple elements : ").split()))
+     container = list(map(int,input("Enter list elements : ").split()))
       
 def OddListCount():
     odd_count = 0
@@ -12,7 +12,7 @@ def EvenListCount():
     even_count = 0
     for i in container: 
           if(i%2==0): even_count+=1 
-    print("The number odd numbers in the list is", even_count)
+    print("The number even numbers in the list is", even_count)
       
 Input()
 OddListCount()
